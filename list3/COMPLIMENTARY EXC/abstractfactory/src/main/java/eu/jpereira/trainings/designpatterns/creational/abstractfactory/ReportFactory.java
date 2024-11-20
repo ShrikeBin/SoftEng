bbdb0f34c91037795ac5e35921360033fc8630b0
ShrikeBin <151054057+ShrikeBin@users.noreply.github.com>
2024-11-20 20:17:28 +0100

    Update ReportFactory.java

--- list3/COMPLIMENTARY EXC/abstractfactory/src/main/java/eu/jpereira/trainings/designpatterns/creational/abstractfactory/ReportFactory.java
+++ list3/COMPLIMENTARY EXC/abstractfactory/src/main/java/eu/jpereira/trainings/designpatterns/creational/abstractfactory/ReportFactory.java
@@ -1,6 +1,6 @@
 package eu.jpereira.trainings.designpatterns.creational.abstractfactory;
 
-// rwlodarczyk: Added interface for the abstract factory pattern
+// Added interface for the abstract factory pattern
 
 public interface ReportFactory {
     ReportBody createBody();
